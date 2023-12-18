2337ecb6d8fe13077d630873b808360589c51f42
hareldamti <94783416+hareldamti@users.noreply.github.com>
2023-12-18 11:49:33 +0200

    internal host on render

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.get("/backup", async (req, res) => {
 });
 
 app.listen(3002, async () => {
-  db = await Database.connect();
+  db = await Database.connect(true);
   courses = await readFile("courses");
   console.log("server started");
 });
